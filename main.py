e9c56ec3329cca1c63e6cebd3a6bf46299e9f12d
kumazirix <kumazirix@gmail.com>
2026-07-30 09:11:01 -0400

    Integrate OpenWeather API and remove email features
    Refactored code to use OpenWeather API for weather data retrieval. Removed email functionality and birthday checks.

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
+from twilio.rest import Client
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+api_key = os.environ.get("OWN_API_KEY")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+MY_LAT =  33.7490
+MY_LONG =  84.3880
+OW_ENDPOINT = "http://api.openweathermap.org/data/2.5/forecast?"
+# http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "units": "imperial",
+    "cnt": 4,
+}
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+response = requests.get(OW_ENDPOINT, params = parameters)
+response.raise_for_status()
+data = response.json()
+print(data)
+
+http_code = data["cod"]
+print(http_code)
+
+weather_list = data["list"]
+time_cnt = data["cnt"]
+
+for num in range(4):
+    weather_item = weather_list[num]
+    weather_dt = weather_item["dt"]
+    weather_dt_txt = weather_item["dt_txt"]
+    weather_id = int(weather_item["weather"][0]["id"])
+    weather_desc = weather_item["weather"][0]["description"]
+    if weather_id < 700:
+        print(f"{weather_dt_txt} - Bring an umbrella")
